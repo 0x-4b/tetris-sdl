28161717356c88f790f32ee46f27b85c74c03f7d
0x-4b <kareem.m.dev@gmail.com>
2025-07-13 18:56:51 +0400

    Implemented the methods

--- src/Pieces.cpp
+++ src/Pieces.cpp
@@ -288,3 +288,19 @@ int mPiecesInitialPosition[7][4][2] = {
         {-2, -2},
     },
 };
+
+/* --------------------------------------------------- */
+
+int Pieces::GetBlockType(int pPiece, int pRotation, int pX, int pY)
+{
+    return mPieces[pPiece][pRotation][pX][pY];
+}
+int Pieces::GetXInitialPosition(int pPiece, int pRotation)
+{
+    return mPiecesInitialPosition[pPiece][pRotation][0];
+}
+int Pieces::GetYInitialPosition(int pPiece, int pRotation)
+{
+
+    return mPiecesInitialPosition[pPiece][pRotation][1];
+}
