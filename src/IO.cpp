b067e6f6c716a3189c886ed6fefafbede7fdc70f
0x-4b <kareem.m.dev@gmail.com>
2025-07-14 17:53:50 +0400

    Finished the init method implementation

--- src/IO.cpp
+++ src/IO.cpp
@@ -19,9 +19,42 @@ IO::~IO()
 
     SDL_Quit();
 }
+
 bool IO::Init()
 {
+
+    bool success = true;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Could not initialize SDL. SDL_Error: " << SDL_GetError() << '\n';
+        success = false;
+    }
+    else
+    {
+        mWindow = SDL_CreateWindow("Tetris SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, mWidth, mHeight, SDL_WINDOW_SHOWN);
+
+        if (mWindow == nullptr)
+        {
+            std::cout << "Could not create a window. SDL_Error: " << SDL_GetError() << '\n';
+
+            success = false;
+        }
+        else
+        {
+            mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+            if (mRenderer == nullptr)
+            {
+
+                std::cout << "Could not create a renderer. SDL_Error: " << SDL_GetError() << '\n';
+
+                success = false;
+            }
+        }
+    }
+    return success;
 }
+
 void IO::ClearScreen() {}
 
 void IO::DrawRectangle(int pX1, int pY1, int pX2, int pY2, SDL_Color pC)
