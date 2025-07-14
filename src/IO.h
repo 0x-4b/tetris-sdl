c9eee3abbce5977e29a3cf50e95cad0f823e3d13
0x-4b <kareem.m.dev@gmail.com>
2025-07-14 17:43:35 +0400

    Updated constructor declaration + added a method

--- src/IO.h
+++ src/IO.h
@@ -6,10 +6,11 @@
 class IO
 {
 public:
-    IO();
+    IO(int pWidth, int pHeight);
     ~IO();
     void ClearScreen();
     void DrawRectangle(int pX1, int pY1, int pX2, int pY2, SDL_Color pC);
+    bool Init();
     void Present();
     int PollEvent(SDL_Event &e);
     int isKeyDown(SDL_Scancode pKey);
