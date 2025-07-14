e31f17d6ed2310cce800f2027d2b554ace055978
0x-4b <kareem.m.dev@gmail.com>
2025-07-15 00:00:31 +0400

    Fixed iteration in store piece

--- src/Board.cpp
+++ src/Board.cpp
@@ -20,13 +20,15 @@ void Board::InitBoard()
 
 void Board::StorePiece(int pX, int pY, int pPiece, int pRotation)
 {
-    for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; ++i1, ++i2)
+    for (int i1 = pX, i2 = 0; i2 < PIECE_BLOCKS; ++i1, ++i2)
     {
-        for (int j1 = pY, j2 = 0; j2 < pY + PIECE_BLOCKS; ++j1, ++j2)
+        for (int j1 = pY, j2 = 0; j2 < PIECE_BLOCKS; ++j1, ++j2)
+        {
             if (mPieces->GetBlockType(pPiece, pRotation, j2, i2) != 0)
             {
                 mBoard[i1][j1] = POS_FILLED;
             }
+        }
     }
 }
 
