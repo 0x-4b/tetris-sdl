04ce379566a8e051b7c6b0f404a8a9f61e44e860
0x-4b <kareem.m.dev@gmail.com>
2025-07-15 00:12:47 +0400

    Probably fixed main

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,10 @@
 
 int main(int argc, char* argv[])
 {
-    IO mIO(800, 600);
+    IO mIO(800, 600); // Use a large enough window
+    if (!mIO.Init()) {
+        return 1;
+    }
     int mScreenHeight = mIO.GetScreenHeight();
 
     Pieces mPieces;
@@ -11,68 +14,63 @@ int main(int argc, char* argv[])
 
     Uint32 mTime1 = SDL_GetTicks();
 
-    bool running = true;
-    while (running)
+    while (!mIO.isKeyDown(SDL_SCANCODE_ESCAPE))
     {
         mIO.ClearScreen();
         mGame.DrawScene();
         mIO.Present();
 
         SDL_Event e;
-        // --- Input ---
-        int mKey = mIO.PollEvent(e);
-
-        switch (mKey)
-        {
-            case SDLK_RIGHT:
-                if (mBoard.IsPossibleMovement(mGame.mPosX + 1, mGame.mPosY, mGame.mPiece, mGame.mRotation))
-                    mGame.mPosX++;
-                break;
-            case SDLK_LEFT:
-                if (mBoard.IsPossibleMovement(mGame.mPosX - 1, mGame.mPosY, mGame.mPiece, mGame.mRotation))
-                    mGame.mPosX--;
-                break;
-            case SDLK_DOWN:
-                if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation))
-                    mGame.mPosY++;
-                break;
-            case SDLK_x:
-                while (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation))
-                    mGame.mPosY++;
-                mBoard.StorePiece(mGame.mPosX, mGame.mPosY - 1, mGame.mPiece, mGame.mRotation);
-                mBoard.DeletePossibleLines();
-                if (mBoard.IsGameOver())
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) return 0;
+            if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym)
                 {
-                    running = false;
+                case SDLK_RIGHT:
+                    if (mBoard.IsPossibleMovement(mGame.mPosX + 1, mGame.mPosY, mGame.mPiece, mGame.mRotation))
+                        mGame.mPosX++;
+                    break;
+                case SDLK_LEFT:
+                    if (mBoard.IsPossibleMovement(mGame.mPosX - 1, mGame.mPosY, mGame.mPiece, mGame.mRotation))
+                        mGame.mPosX--;
+                    break;
+                case SDLK_DOWN:
+                    if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation))
+                        mGame.mPosY++;
+                    break;
+                case SDLK_x:
+                    while (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation))
+                        mGame.mPosY++;
+                    mBoard.StorePiece(mGame.mPosX, mGame.mPosY, mGame.mPiece, mGame.mRotation);
+                    mBoard.DeletePossibleLines();
+                    if (mBoard.IsGameOver()) {
+                        SDL_Delay(3000);
+                        return 0;
+                    }
+                    mGame.CreateNewPiece();
+                    break;
+                case SDLK_z:
+                    if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY, mGame.mPiece, (mGame.mRotation + 1) % 4))
+                        mGame.mRotation = (mGame.mRotation + 1) % 4;
+                    break;
+                case SDLK_ESCAPE:
+                    return 0;
                 }
-                mGame.CreateNewPiece();
-                break;
-            case SDLK_z:
-                if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY, mGame.mPiece, (mGame.mRotation + 1) % 4))
-                    mGame.mRotation = (mGame.mRotation + 1) % 4;
-                break;
-            case SDLK_ESCAPE:
-                running = false;
-                break;
-            default:
-                break;
+            }
         }
 
-        // --- Vertical movement ---
         Uint32 mTime2 = SDL_GetTicks();
         if ((mTime2 - mTime1) > WAIT_TIME)
         {
             if (mBoard.IsPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation))
-            {
                 mGame.mPosY++;
-            }
             else
             {
                 mBoard.StorePiece(mGame.mPosX, mGame.mPosY, mGame.mPiece, mGame.mRotation);
                 mBoard.DeletePossibleLines();
-                if (mBoard.IsGameOver())
-                {
-                    running = false;
+                if (mBoard.IsGameOver()) {
+                    SDL_Delay(3000);
+                    return 0;
                 }
                 mGame.CreateNewPiece();
             }
