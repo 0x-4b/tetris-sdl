26c0d274120ff4eac7da0c2487a53406084b4f33
0x-4b <kareem.m.dev@gmail.com>
2025-07-15 18:07:07 +0400

    Finished font class declaration in header file

---
+++ src/Font.h
@@ -0,0 +1,25 @@
+#ifndef _FONT_
+#define _FONT_
+
+#include <SDL2/SDL_ttf.h>
+#include <string>
+
+class Font
+{
+public:
+    Font();
+    ~Font();
+
+    bool init();
+    bool LoadFromRenderedText(std::string pText, SDL_Color pColor);
+    bool LoadMedia(std::string pText);
+
+    void render(int pX, int pY, SDL_Rect *pClip, SDL_Renderer pRenderer);
+
+    int GetWidht();
+    int GetHeight();
+
+private:
+    int mWidth, mHeight;
+};
+#endif // _FONT_
