1510ce61309e5bb3247ca92ddcd833ae94e81d37
0x-4b <kareem.m.dev@gmail.com>
2025-07-15 18:16:05 +0400

    Added destructor

--- src/Font.cpp
+++ src/Font.cpp
@@ -6,6 +6,13 @@ Font::Font()
     mHeight = 0;
 }
 
+Font::~Font()
+{
+    mWidth = 0;
+    mHeight = 0;
+    TTF_Quit();
+}
+
 bool Font::init()
 {
     if (TTF_Init() == -1)
@@ -15,7 +22,9 @@ bool Font::init()
     }
     return true;
 }
-bool Font::LoadFromRenderedText(std::string pText, SDL_Color pColor) {}
+bool Font::LoadFromRenderedText(std::string pText, SDL_Color pColor)
+{
+}
 bool Font::LoadMedia(std::string pText) {}
 
 void Font::render(int pX, int pY, SDL_Rect *pClip, SDL_Renderer *pRenderer) {}
